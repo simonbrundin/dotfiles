a8572488252d6150858abe1a2272b6bde0a3d357
Simon Brundin <simonbrundin@gmail.com>
2025-11-03 10:33:01 +0100

    restructure kanata config

--- start_dotfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting tmuxinator Dotfiles at $(date)" >> ~/kanata_log.txt
-export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-alacritty -e 'tmux attach-session -t Dotfiles || tmuxinator start Dotfiles' &
-echo "Done" >> ~/kanata_log.txt
