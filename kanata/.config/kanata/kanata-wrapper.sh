0653415ae768389884701ff3d06e9b9236e3a8b7
Simon Brundin <simonbrundin@gmail.com>
2026-05-22 11:25:30 +0200

    update

--- kanata/.config/kanata/kanata-wrapper.sh
+++ kanata/.config/kanata/kanata-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export HYPRLAND_INSTANCE_SIGNATURE=$(ls -la /run/user/1000/hypr/ 2>/dev/null | grep -oP '^[^\s_]+' | tail -1)
+export HYPRLAND_INSTANCE_SIGNATURE=$(ls /run/user/1000/hypr/ 2>/dev/null)
 exec /home/simon/.local/bin/kanata -c /home/simon/.config/kanata/kanata.kbd "$@"
