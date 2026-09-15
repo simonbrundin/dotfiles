4d32f75dc458861c09939d097f590db1435fa6ac
Simon Brundin <simonbrundin@gmail.com>
2026-09-15 17:50:34 +0200

    chore: update kanata and hypr input config

--- kanata/.config/kanata/kanata-wrapper.sh
+++ kanata/.config/kanata/kanata-wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export PATH="/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
 export HYPRLAND_INSTANCE_SIGNATURE=$(find /run/user/1000/hypr -mindepth 1 -maxdepth 1 -type d -printf '%T@ %f\n' 2>/dev/null | sort -n | tail -1 | cut -d' ' -f2-)
 exec /home/simon/.local/bin/kanata -c /home/simon/.config/kanata/kanata.kbd "$@"
